693633efbd5893194d1312bc65bbb9e2473f692b
Flavien <petit.lepton@gmail.com>
2018-07-03 09:39:01 +0800

    Correct function docstring

--- align_left.py
+++ align_left.py
@@ -1,13 +1,12 @@
 def align_left(df, row_heading=False, columns=None):
     '''This function aligns the column headers to the left as well
-    as the elements of the columns provided by columns_left or the
-    index column.
+    as provided columns.
 
     Args:
         df (pandas.DataFrame): the input DataFrame
         row_heading (bool, default False): if True, align left the
             index column
-        columns_left (list, default None): list of columns to align left
+        columns (list, default None): list of columns to align left
     Returns:
         pandas.io.formats.style.Styler'''
 
